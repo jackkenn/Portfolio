7f347dc3e90e0e244558e9c55ec1e298999fda99
jackkenn <jackdkennedyjack@gmail.com>
2025-01-22 23:56:06 -0600

    added some page content

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         </nav>
         {children}          
       </body>
+      {/* todo footer */}
     </html>
   );
 }
--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,8 +2,13 @@
 export default function Home() {
   return (
     <div className="container">
-      <div className="row-lg">
-        <div className="col-lg">
+      <div className="row">
+        <div className="col">
+          <h1>Jack Kennedy</h1>
+          <p>Full Stack Developer</p>
+        </div>
+        <div className="col">
+          <img src="images/otang.jpg" alt="Profile Picture" />
         </div>
       </div>
     </div>
